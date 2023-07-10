808ea2761a766cce7eb0ed95ef52448c01fc0524
Ken Wu <kenwu1009us@gmail.com>
2023-07-10 11:51:49 -0400

    clean data

---
+++ data_quality_evaluation.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import os
+
+from JSONProcessor import JSONProcessor
+from pathlib import Path
+
+
+def get_dataframe(filename):
+    data_path = Path('.') / 'data'
+    json_processor = JSONProcessor(data_path)
+
+    if filename in os.listdir(data_path):
+        json_processor.process_json_file(filename)
+        json_data = json_processor.get_json_data(filename)
+
+    else:
+        print("File not found.")
+
+    # json_normalize function is used to convert the list of JSON objects into a DataFrame.
+    # This function flattens the nested JSON structure and creates
+    # a tabular representation suitable for analysis.
+    df = pd.json_normalize(json_data)
+    return df
+
+
+def data_cleaning(df):
+
+    # Inconsistent column names
+    df.rename(columns={'_id.$oid': 'id',
+                       'createDate.$date': 'createDate',
+                       'dateScanned.$date': 'dateScanned',
+                       'finishedDate.$date': 'finishedDate',
+                       'modifyDate.$date': 'modifyDate',
+                       'pointsAwardedDate.$date': 'pointsAwardedDate',
+                       'pointsEarned.$date': 'pointsEarned',
+                       'purchaseDate.$date': 'purchaseDate'}, inplace=True)
+
+
+    # Missing values
+    missing_values = df.isnull().sum()
+    print('\nMissing values:')
+    print(missing_values)
+
+    # Inconsistent formats or data types
+    df['createDate'] = pd.to_datetime(df['createDate'], unit='ms').dt.date
+    df['dateScanned'] = pd.to_datetime(df['dateScanned'], unit='ms').dt.date
+    df['finishedDate'] = pd.to_datetime(df['finishedDate'], unit='ms').dt.date
+    df['modifyDate'] = pd.to_datetime(df['modifyDate'], unit='ms').dt.date
+    df['pointsAwardedDate'] = pd.to_datetime(df['pointsAwardedDate'],
+                                             unit='ms').dt.date
+    df['purchaseDate'] = pd.to_datetime(df['purchaseDate'], unit='ms').dt.date
+
+
+    # Handling missing values
+    bonus_points_col = df['bonusPointsEarned']
+    points_earned_col = df['pointsEarned']
+
+    if bonus_points_col.dtype.kind in 'iufc':
+        # Column is numeric
+        mean_bonus_points = bonus_points_col.mean()
+        df['bonusPointsEarned'].fillna(mean_bonus_points, inplace=True)
+    else:
+        # Column is categorical or contains string values
+        most_frequent_bonus_points = bonus_points_col.mode().iloc[0]
+        df['bonusPointsEarned'].fillna(most_frequent_bonus_points, inplace=True)
+
+    if points_earned_col.dtype.kind in 'iufc':
+        # Column is numeric
+        mean_points_earned = points_earned_col.mean()
+        df['pointsEarned'].fillna(mean_points_earned, inplace=True)
+    else:
+        # Column is categorical or contains string values
+        most_frequent_points_earned = points_earned_col.mode().iloc[0]
+        df['pointsEarned'].fillna(most_frequent_points_earned, inplace=True)
+
+    print(df)
+
+
+def main():
+    # filename = input("Enter the JSON filename to process: ")
+    filename = 'receipts.json'
+    brands_df = get_dataframe(filename)
+    data_cleaning(brands_df)
+
+
+
+if __name__ == "__main__":
+    main()
